948ca9651df49e5e65c6ffd20b33b7712257271e
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-12-22 22:54:57 +0100

    singlefruit

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SinglefruitComponent } from "./singlefruit/singlefruit.component";
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SinglefruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
@@ -12,8 +13,8 @@ export class FruitlistComponent {
 
   howItWorks = "not bad";
 
-  fontColorGood = 'green';
-  fontColorBad = 'red';
+  // fontColorGood = 'green';
+  // fontColorBad = 'red';
 
   fruitlist = [
     {
---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss'
+})
+export class SinglefruitComponent {
+  // @Input()fruitnumber = 0;
+  @Input("testNumber")fruitnumber = 0;
+}
